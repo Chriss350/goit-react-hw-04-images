cf463ce03346f9feb83dd2d9e44a7fc0564d8e41
qudlaty <106681377+Chriss350@users.noreply.github.com>
2023-03-22 13:16:21 +0100

    init

--- src/components/Button/LoadMore.jsx
+++
@@ -1,9 +0,0 @@
-export const LoadMore = ({ onClick }) => {
-  return (
-    <div className="col-md-12 mb-3 text-center">
-      <button className="btn btn-secondary btn-lg" onClick={onClick}>
-        Load More
-      </button>
-    </div>
-  );
-};
--- src/components/ImageGallery/ImageGallery.jsx
+++
@@ -1,9 +0,0 @@
-import css from './ImageGallery.module.css';
-
-export const ImageGallery = ({ children, openModal }) => {
-  return (
-    <ul className={css.gallery} onClick={openModal}>
-      {children}
-    </ul>
-  );
-};
